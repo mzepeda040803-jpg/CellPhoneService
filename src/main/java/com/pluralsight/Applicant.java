82d8008898c7434baf6e22c617833ffd515c965b
m.zepeda040803-jpg <m.zepeda040803@gmai.com>
2025-10-02 10:36:01 -0400

    Second Commit

---
+++ src/main/java/com/pluralsight/Applicant.java
@@ -0,0 +1,4 @@
+package com.pluralsight;
+
+public class Applicant extends CellPhone {
+}
--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,4 +1,74 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        CellPhone cellPhone1 = new CellPhone();
+
+        //cellPhone should match the class name from list of getters/setters
+
+        cellPhone1.setSerialNumber("12345 - 67890");
+        cellPhone1.setModel("iphone 14");
+        cellPhone1.setCarrier("Verizon");
+        cellPhone1.setPhoneNumber("111-111-1111");
+        cellPhone1.setOwner("Dana Wyatt");
+
+
+        System.out.println("What is the serial number?");
+        System.out.println(cellPhone1.getSerialNumber());
+
+
+        System.out.println("What model is the phone?");
+        System.out.println(cellPhone1.getModel());
+
+
+        System.out.println("Who is the carrier?");
+        System.out.println(cellPhone1.getCarrier());
+
+
+        System.out.println("What is the phone number?");
+        System.out.println(cellPhone1.getPhoneNumber());
+
+
+        System.out.println("Who is the owner of the phone?");
+        System.out.println(cellPhone1.getOwner());
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // serialNumber (ex: 1000000 - 9999999)
+// model (ex: iPhone X)
+ //carrier (ex: AT&T)
+ //phoneNumber (ex: 800-555-5555)
+ //owner (ex: Dana Wyatt
+
+
+
+
 }
